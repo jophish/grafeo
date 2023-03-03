f3894b7d68271d3d4a8391a8d591647466679d6b
Joseph Bergeron <jophish126@gmail.com>
2023-03-03 14:24:46 -0500

    Update parameters

--- plotter/generators/Parameters.py
+++ plotter/generators/Parameters.py
@@ -1,15 +1,33 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from collections import OrderedDict
-from enum import EnumType, StrEnum, auto
-from typing import Any, Generic, Literal, TypeVar
+from enum import Enum
+from typing import Any, Generic, TypeVar
 
 
-class ParamType(StrEnum):
+class ParamNotFoundException(Exception):
+    """Exception raised when parameter is not found."""
+
+    pass
+
+
+class WrongParamTypeException(Exception):
+    """Exception raised when wrong parameter type is used."""
+
+    pass
+
+
+class InvalidParamValueException(Exception):
+    """Exception raised when a parameter value is invalid."""
+
+    pass
+
+
+class ParamType(Enum):
     """The ParamType class is used to differentiate between parameter types."""
 
-    Int = auto()
-    Float = auto()
-    Enum = auto()
+    Int = int
+    Float = float
+    Enum = str
 
 
 T = TypeVar("T")
@@ -50,6 +68,8 @@ class GeneratorParamGroup:
         """
         Return a nested dictionary containing the current parameter values.
 
+        The output of this method returns a shape compatible with `set_dict_values`.
+
         :return: Nested dictionary containing parameter values
         """
         param_dict = {}
@@ -59,6 +79,45 @@ class GeneratorParamGroup:
             else:
                 param_dict[name] = param.value
         return param_dict
+
+    def set_dict_values(self, param_values: dict[str, Any]):
+        """
+        Set the current values of parameters in this group.
+
+        Accepts a nested dictionary containing desired parameter values.
+
+        The input of this method accepts a shape compatible with the output
+        of `get_dict_values`.
+
+        If a provided parameter is not found, a :class:`ParamNotFoundException` is thrown.
+        If a provided parameter is found, but its type differs from the provided parameter value,
+        a :class:`WrongParamTypeException` is thrown.
+        If a provided parameter value fails validation, a :class:`InvalidParamValueException` is thrown.
+
+        :raises ParamNotFoundException: Parameter name not found
+        :raises WrongParamTypeException: Parameter to set has wrong type
+        :raises InvalidParamValueException: Parameter fails validation
+
+        :param param_values: Nested dictionary containing parameter values
+        """
+        for name, param in param_values.items():
+            if name not in self.params:
+                raise ParamNotFoundException(f'Parameter "{name}" not found')
+
+            current_param = self.params[name]
+            if isinstance(param, dict):
+                if not isinstance(current_param, GeneratorParamGroup):
+                    expected_type = current_param.param_type
+                    raise WrongParamTypeException(
+                        f"Wrong param type for {name}. Expected {expected_type}, got dict"
+                    )
+                current_param.set_dict_values(param)
+            else:
+                if not isinstance(current_param, GeneratorParam):
+                    raise WrongParamTypeException(
+                        f"Wrong param type for {name}. Expected dict, got {type(param)}"
+                    )
+                current_param.value = param
 
 
 class GeneratorParam(ABC, Generic[T]):
@@ -90,13 +149,50 @@ class GeneratorParam(ABC, Generic[T]):
         """
         self.name: str = name
         self.description: str = description
-        self.value: T = default_value
+        self._value: T = default_value
         self.default_value: T = default_value
         self.param_type: ParamType = param_type
 
     def reset(self):
         """Reset the parameter value to its default."""
-        self.value = self.default_value
+        self._value = self.default_value
+
+    @property
+    def value(self):
+        """Get the current value of this parameter."""
+        return self._value
+
+    @value.setter
+    def value(self, value: Any):
+        """
+        Set the value of the parameter.
+
+        Enforces typechecking and validation.
+        Throws a :class:`WrongParamTypeException` if the provided value has an incorrect type.
+        Throws a :class:`InvalidParamValueException` if the provided value fails validation.
+
+        :raises WrongParamTypeException: Parameter value has incorrect type
+        :raises InvalidParamValueException: Validation checks fail
+        """
+        if not type(value) == self.param_type:
+            raise WrongParamTypeException(
+                f"Wrong param type for {self.name}. Expected {self.param_type}, got {type(value)} ({value})"
+            )
+        self._validate_param_value(value)
+        self._value = value
+
+    @abstractmethod
+    def _validate_param_value(self, value: T):
+        """
+        Validate a param value against custom semantics.
+
+        Raises an :class:`InvalidParamValueException` when checks fail.
+
+        :raises InvalidParamValueException: Validation checks fail
+
+        :param value: The value to validate
+        """
+        pass
 
 
 class IntParam(GeneratorParam[int]):
@@ -129,6 +225,21 @@ class IntParam(GeneratorParam[int]):
         super().__init__(name, description, ParamType.Int, default_value)
         self.min_value: int = min_value
         self.max_value: int = max_value
+
+    def _validate_param_value(self, value: int):
+        """
+        Validate that a value is within the parameter's bounds.
+
+        Raises a :class:`InvalidParamValueException` when checks fail.
+
+        :raises InvalidParamValueException: Value is outside parameter's bounds
+
+        :param value: The value to validate
+        """
+        if not (self.min_value <= value <= self.max_value):
+            raise InvalidParamValueException(
+                f"Param value must be within {self.min_value}-{self.max_value}. Got {value}."
+            )
 
 
 class FloatParam(GeneratorParam[float]):
@@ -165,6 +276,21 @@ class FloatParam(GeneratorParam[float]):
         self.min_value: float = min_value
         self.max_value: float = max_value
 
+    def _validate_param_value(self, value: float):
+        """
+        Validate that a value is within the parameter's bounds.
+
+        Raises a :class:`InvalidParamValueException` when checks fail.
+
+        :raises InvalidParamValueException: Value is outside parameter's bounds
+
+        :param value: The value to validate
+        """
+        if not (self.min_value <= value <= self.max_value):
+            raise InvalidParamValueException(
+                f"Param value must be within {self.min_value}-{self.max_value}. Got {value}."
+            )
+
 
 class EnumParam(GeneratorParam[str]):
     """
@@ -187,3 +313,18 @@ class EnumParam(GeneratorParam[str]):
         """
         super().__init__(name, description, ParamType.Enum, default_value)
         self.options: list[str] = options
+
+    def _validate_param_value(self, value: str):
+        """
+        Validate that a value is within the parameter's bounds.
+
+        Raises a :class:`InvalidParamValueException` when checks fail.
+
+        :raises InvalidParamValueException: Value is outside parameter's bounds
+
+        :param value: The value to validate
+        """
+        if value not in self.options:
+            raise InvalidParamValueException(
+                f"Param value must be one of {self.options}. Got {value}."
+            )
