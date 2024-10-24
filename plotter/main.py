5804b9c843c06526d49c17af2a0afd18a5d27f82
Joseph Bergeron <jophish126@gmail.com>
2024-10-23 21:14:34 -0400

    change name

--- plotter/main.py
+++ plotter/main.py
@@ -1,23 +1,4 @@
-from plotter.config.ConfigManager import ConfigManager
-from plotter.generators.GeneratorManager import GeneratorManager
-from plotter.gui.gui import setup_gui
 from plotter.gui.Gui import Gui
-
-
-def main():
-    """Run the program."""
-    generator_manager = GeneratorManager()
-    config_manager = ConfigManager(generator_manager.get_generator_defaults())
-    generator_manager.set_all_generator_param_values(
-        config_manager.get_all_generator_param_values()
-    )
-    generator_manager.set_current_generator(config_manager.get_current_generator())
-    managers = {
-        "generator_manager": generator_manager,
-        "config_manager": config_manager,
-    }
-    setup_gui(managers)
-
 
 gui = Gui()
 gui.start()
